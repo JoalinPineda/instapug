0adbf4ebe67158279df19d4e164ddfe0b1cb748a
JoalinPineda <joalin.pineda@gmail.com>
2021-06-11 11:36:14 -0500

    first commit

---
+++ src/components/Footer.svelte
@@ -0,0 +1,14 @@
+<style>
+    .Footer {
+      font-size: 11px;
+      letter-spacing: 1px;
+      font-weight: normal;
+      color: #c7c7c7;
+    }
+</style>
+
+<div class="Footer">
+    <div class="Footer-container">
+        <div class="footer-copy">© 2020 INSTAPUG BY JOALIN PINEDA</div>
+    </div>
+</div>
---
+++ src/components/Stories.svelte
@@ -0,0 +1,86 @@
+<style>
+    .Stories {
+      border: 1px solid rgba(219, 219, 219, 1);
+      border-radius: 4px;
+      margin: 1em 0;
+      padding: 0.5em 1em;
+      background-color: white;
+    }
+    .Stories-item img {
+      width: 32px;
+      height: 32px;
+      border-radius: 50%;
+    }
+    .Stories-item {
+      display: flex;
+      align-items: center;
+      margin: 0 0 0.5em 0;
+    }
+    .Stories-item h2 {
+      font-size: 14px;
+      color: black;
+      margin: 0 0 0 0.5em;
+    }
+    .Stories-item h2 span {
+      display: block;
+      font-size: 9px;
+      text-transform: uppercase;
+      color: gray;
+    }
+    .Stories-head {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0.5em 0 0 0;
+    }
+    .Stories-head h2 {
+      font-size: 12px;
+      font-weight: 600;
+    }
+    .Stories-head span {
+      font-size: 12px;
+      font-weight: normal;
+      color: black;
+    }
+    .Stories-items {
+      margin: 0.5em 0 0.5em 0;
+    }
+    .Stories-item-box {
+      width: 32px;
+      height: 32px;
+      border: double 2px transparent;
+      border-radius: 100%;
+      background-image: linear-gradient(#fff, #fff),
+        radial-gradient(circle at top left, #f09433, #bc1888);
+      background-origin: border-box;
+      background-clip: content-box, border-box;
+    }
+</style>
+<div class="Stories">
+    <div class="Stories-contai">
+        <div class="Stories-head">
+            <h2>Historias</h2>
+            <span>Ver todas</span>
+        </div>
+        <div class="Stories-items">
+            <div class="Stories-item">
+                <div class="Stories-item-box">
+                    <img src="https://inba.gob.mx/multimedia/prensa/galerias/14994/14994-GAL-whatsapp_image_2021-02-11_at_10.06.32_(1).jpeg" alt="" />
+                </div>
+                <h2>
+                    joalinpineda
+                    <span>10 horas antes</span>
+                </h2>
+            </div>
+            <div class="Stories-item">
+              <div class="Stories-item-box">
+                  <img src="https://scontent.fmex1-1.fna.fbcdn.net/v/t1.18169-9/20881858_1572479072804095_5516195461758080880_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=a4a2d7&_nc_ohc=dkIuoGXpcAMAX_nPbLf&_nc_ht=scontent.fmex1-1.fna&oh=870df8ef2d0ac15136cf9a62ceefba99&oe=60C8DA08" alt="" />
+              </div>
+              <h2>
+                  gabsscas
+                  <span>5 horas antes</span>
+              </h2>
+          </div>
+        </div>
+    </div>
+</div>
